82dad29ea42bad561b8ef704551800c0072179e5
Nate Deisler <ineanderthals@Nates-MacBook-Pro.local>
2019-01-21 11:30:57 -0500

    done with API, I think

--- main.js
+++ main.js
@@ -33,69 +33,64 @@ function fetchCharacters(e) {
 }
 
 function displayResults(json) {
-    let episode1 = json.episode[0];
-    fetch(episode1).then(function(results) {
-        console.log(results);
-        return results.json();
-    }).then(function(json) {
-        console.log(json.episode);  
-    });
-
-    
-    
     while(div1.firstChild) {
         div1.removeChild(div1.firstChild);   
     }
 
-    
-    
-   
+    let episode1 = json.episode[0];
+    fetch(episode1).then(function(results) {
+        console.log(results);
+        return results.json();
+    }).then(function(json2) {
+        // console.log(json2.episode); 
+        let ep = json2.episode;
 
-    console.log(json);
-    let characters = json.name;
-    let characterID = json.id;
+
+        //Trying to display character stats
+        let div2 = document.createElement("div");
+        let img = document.createElement("img");    
+        let heading = document.createElement("h2");
+        let status = document.createElement("p");
+        let gender = document.createElement("p");
+        let species = document.createElement("p");
+        let origin = document.createElement("p");
+        let episode = document.createElement("p");
+
+
+        img.src = json.image;
+        heading.innerHTML = json.name;
+        status.innerHTML = "<p><span>Status</span>: <em>"  + characterStatus + "</em></p>";
+        gender.innerHTML = "<p><span>Gender</span>: <em>" + characterGender + "</em></p><hr>";
+        species.innerHTML = "<p><span>Species</span>: <em>" + characterSpecies + "</em></p><hr>";
+        episode.innerHTML = "<p><span>First Appeared</span>: <em>" + ep + "</em></p><hr>";
+        origin.innerHTML = "<p><span>Origin</span>: <em>" + characterOrigin.name + "</em></p><hr>";
+
+
+        // div.setAttribute("class", "slide-up");
+        div1.appendChild(div2);
+        div2.setAttribute("class", "cardbox");
+        
+        div2.appendChild(img);
+        div2.appendChild(heading);
+        
+        div2.appendChild(species);
+        div2.appendChild(gender);
+        div2.appendChild(origin);
+        div2.appendChild(episode);
+        div2.appendChild(status);
+
+
+        
+    });
+
+    // let characters = json.name;
+    // let characterID = json.id;
     let characterStatus = json.status;
     let characterGender = json.gender;
     let characterSpecies = json.species;
     let characterOrigin = json.origin;
-    
-    
-    
-    
-    console.log(characters);
-    console.log(characterID);
-    console.log(characterStatus);
-    // console.log(firstEpisode);
 
-    //Trying to display character stats
-    let div2 = document.createElement("div");
-    let img = document.createElement("img");    
-    let heading = document.createElement("h2");
-    let status = document.createElement("p");
-    let gender = document.createElement("p");
-    let species = document.createElement("p");
-    let origin = document.createElement("p");
-    let episode = document.createElement("p");
-    
- 
-    img.src = json.image;
-    heading.innerHTML = json.name;
-    status.innerHTML = "<p><span>Status</span>: <em>"  + characterStatus + "</em></p><hr>";
-    gender.innerHTML = "<p><span>Gender</span>: <em>" + characterGender + "</em></p><hr>";
-    species.innerHTML = "<p><span>Species</span>: <em>" + characterSpecies + "</em></p><hr>";
-    origin.innerHTML = "<p><span>Origin</span>: <em>" + characterOrigin.name + "</em></p><hr>";
-    episode.innerHTML = "<p><span>First ep</span>: <em>" + ep + "</em></p><hr>";
+}
 
-    // div.setAttribute("class", "slide-up");
-    div1.appendChild(div2);
-    div2.setAttribute("class", "cardbox");
     
-    div2.appendChild(img);
-    div2.appendChild(heading);
     
-    div2.appendChild(species);
-    div2.appendChild(gender);
-    div2.appendChild(origin);
-    div2.appendChild(status);
-    div2.appendChild(episode);
-} 
