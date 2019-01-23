019b992278eeda306dbde57df38aa36ad2d1b622
Nate Deisler <ineanderthals@Nates-MacBook-Pro.local>
2019-01-23 08:17:22 -0500

    conditional for input

--- main.js
+++ main.js
@@ -18,73 +18,76 @@ let characterNumber = document.querySelector("input");
 getInfo.addEventListener("submit", fetchCharacters);
 
 function fetchCharacters(e) {
-    e.preventDefault();
+    if (characterNumber.value > 494 || characterNumber.value === "" || characterNumber.value < 1) {
+        alert("Character doesn't exist. Please choose a character number BETWEEN 1-493!");
+    } else {
+        e.preventDefault();
 
-    // console.log(e);
-    url = baseURL + "character/" + characterNumber.value + "/";
-    // console.log(url)
-    fetch(url).then(function(results) {
-        // console.log(results);
-        return results.json();
-    }).then(function(json) {
-        
-        displayResults(json);
-    });
-}
-
-function displayResults(json) {
-    while(div1.firstChild) {
-        div1.removeChild(div1.firstChild);   
+        // console.log(e);
+        url = baseURL + "character/" + characterNumber.value + "/";
+        // console.log(url)
+        fetch(url).then(function(results) {
+            // console.log(results);
+            return results.json();
+        }).then(function(json) {
+            
+            displayResults(json);
+        });
     }
 
-    let episode1 = json.episode[0];
-    fetch(episode1).then(function(results) {
-        // console.log(results);
-        return results.json();
-    }).then(function(json2) {
-        // console.log(json2.episode); 
-        let ep = json2.episode;
+    function displayResults(json) {
+        while(div1.firstChild) {
+            div1.removeChild(div1.firstChild);   
+        }
+
+        let episode1 = json.episode[0];
+        fetch(episode1).then(function(results) {
+            // console.log(results);
+            return results.json();
+        }).then(function(json2) {
+            // console.log(json2.episode); 
+            let ep = json2.episode;
 
 
-        //Trying to display character stats
-        let div2 = document.createElement("div");
-        let img = document.createElement("img");    
-        let heading = document.createElement("h2");
-        let status = document.createElement("p");
-        let gender = document.createElement("p");
-        let species = document.createElement("p");
-        let origin = document.createElement("p");
-        let episode = document.createElement("p");
+            //Trying to display character stats
+            let div2 = document.createElement("div");
+            let img = document.createElement("img");    
+            let heading = document.createElement("h2");
+            let status = document.createElement("p");
+            let gender = document.createElement("p");
+            let species = document.createElement("p");
+            let origin = document.createElement("p");
+            let episode = document.createElement("p");
 
 
-        img.src = json.image;
-        heading.innerHTML = json.name;
-        status.innerHTML = "<p><span>Status</span>: <em>"  + characterStatus + "</em></p>";
-        gender.innerHTML = "<p><span>Gender</span>: <em>" + characterGender + "</em></p><hr>";
-        species.innerHTML = "<p><span>Species</span>: <em>" + characterSpecies + "</em></p><hr>";
-        episode.innerHTML = "<p><span>First Appeared</span>: <em>" + ep + "</em></p><hr>";
-        origin.innerHTML = "<p><span>Origin</span>: <em>" + characterOrigin.name + "</em></p><hr>";
+            img.src = json.image;
+            heading.innerHTML = json.name;
+            status.innerHTML = "<p><span>Status</span>: <em>"  + characterStatus + "</em></p>";
+            gender.innerHTML = "<p><span>Gender</span>: <em>" + characterGender + "</em></p><hr>";
+            species.innerHTML = "<p><span>Species</span>: <em>" + characterSpecies + "</em></p><hr>";
+            episode.innerHTML = "<p><span>First Appeared</span>: <em>" + ep + "</em></p><hr>";
+            origin.innerHTML = "<p><span>Origin</span>: <em>" + characterOrigin.name + "</em></p><hr>";
 
 
-        // div.setAttribute("class", "slide-up");
-        div1.appendChild(div2);
-        div2.setAttribute("class", "cardbox");
-        
-        div2.appendChild(img);
-        div2.appendChild(heading);
-        
-        div2.appendChild(species);
-        div2.appendChild(gender);
-        div2.appendChild(origin);
-        div2.appendChild(episode);
-        div2.appendChild(status);
-    });
+            // div.setAttribute("class", "slide-up");
+            div1.appendChild(div2);
+            div2.setAttribute("class", "cardbox");
+            
+            div2.appendChild(img);
+            div2.appendChild(heading);
+            
+            div2.appendChild(species);
+            div2.appendChild(gender);
+            div2.appendChild(origin);
+            div2.appendChild(episode);
+            div2.appendChild(status);
+        });
 
-    let characterStatus = json.status;
-    let characterGender = json.gender;
-    let characterSpecies = json.species;
-    let characterOrigin = json.origin;
-
+        let characterStatus = json.status;
+        let characterGender = json.gender;
+        let characterSpecies = json.species;
+        let characterOrigin = json.origin;
+    }
 }
 
     
